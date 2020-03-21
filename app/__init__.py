85180df1a5385a7f2c59f78c9f813ca335e566b2
Vlad Nedelcu <nedelcuvd@gmail.com>
2020-03-21 13:59:03 +0200

    Changed Configurations

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,16 @@ class DevelopmentConfig(object):
         self.ENV = 'Development'
         self.URL_PREFIX = '/api/v1'
         self.BASE_URL = 'https://api.themoviedb.org'
-        
+        self.HOST = '0.0.0.0'
+        self.PORT = '5000'
+
+class ProductionConfig(object):
+    
+    def __init__(self):
+        self.DEBUG = False
+        self.API_KEY = os.environ.get('API_KEY')
+        self.ENV = 'Production'
+        self.URL_PREFIX = '/api/v1'
+        self.BASE_URL = 'https://api.themoviedb.org'
+        self.HOST = '0.0.0.0'
+        self.PORT = '5000'
